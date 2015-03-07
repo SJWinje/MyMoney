c913e874df09a17156a044658f62eb1f6df44bea
Stephen Winje <sjwinje@gmail.com>
2015-03-06 23:08:17 -0600

    Initial Admin and Accounts pages working
    Admin page - Can now add Users with a specified balance (only for
    savings account for now).  Can remove Users.
    Accounts page - can see User accounts and balance at a glance, can
    select User.

--- mymoney.js
+++ mymoney.js
@@ -1,4 +1,4 @@
-AccountsList = new Mongo.Collection('accounts');
+AccountList = new Mongo.Collection('accounts');
 
 Router.route('/', function () {
   this.render('Home', {data: {title: 'My Title'}});
@@ -13,7 +13,7 @@ Router.route('/accounts', function () {
 });
 
 Router.route('/accounts/:_id', function () {
-  var account = Accounts.findOne({_id: this.params._id});
+  var account = AccountList.findOne({_id: this.params._id});
   this.render('ShowAccount', {data: account});
 });
 
@@ -64,10 +64,114 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.addChild.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.addChild.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    },
+    'submit form': function(e) {
+      var userName = e.target.userName.value;
+      var userPassword = e.target.userPassword.value;
+      var userBalance = Number(e.target.userBalance.value);
+      console.log("Add child: " + userName + ", " + userPassword + ", " + userBalance );
+
+      e.preventDefault();
+      var options = {
+        username: userName,
+        password: userPassword
+      };
+      Meteor.call('addUser', options)
+
+      AccountList.insert({
+        userName: userName,
+        type: "savings",
+        balance: userBalance
+      });
+
+      e.target.userName.value = null;
+      e.target.userPassword.value = null;
+      e.target.userBalance.value = null;
+    }
+  });
+
+  Template.Admin.events({
+    'click .remove': function(){
+      var selectedUser = this._id;
+      console.log("User: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (confirm("Are you sure you want to remove player?")) {
+        Meteor.call('removeUser', selectedUser);
+      }
+    }
+  });
+
+  Template.Admin.helpers({
+    user: function() {
+      return AccountList.find();
+    }
+  });
+
+  Template.showAccounts.helpers({
+    user: function() {
+      return AccountList.find();
+    },
+    'selectedUser': function(){
+      var userId = this._id;
+      var selectedUser = Session.get('selectedUser');
+      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (userId == selectedUser) {
+        // return this class so CSS can highlight the selected player
+        return "selected"
+      }
+    },
+  });
+
+  Template.showAccounts.events({
+    'click .user': function(event){
+      var userId = this._id;
+      Session.set('selectedUser', userId);
+    },
+
+  });
+
+  Template.Accounts.helpers({
+    'showSelectedUser': function(){
+      var userId = this._id;
+      var selectedUser = Session.get('selectedUser');
+      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (userId == selectedUser) {
+      }
+      return AccountList.findOne(selectedUser);
+    },
+  });
+
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.methods({
+    'addUser': function(options){
+      Accounts.createUser(options);
+
+    },
+    'removeUser': function(user) {
+      AccountList.remove(user);
+//      Accounts.remove(user);
+    }
+  });
+
 }
