6293bb57636f889c4920786d901c849e1f5103a1
Stephen Winje <sjwinje@gmail.com>
2015-03-09 18:32:39 -0500

    Completed Sprint 2
    Stories completed:
    * (1) As a parent, I want it to be easy to credit or debit a child's
    account so a child's account stays up to date.
    * (5) As a user, I want to see the list of transactions for each
    account so I can reconcile the balance in the account.
    * (2) As a user, I want to delete all transactions when a user is
    removed so I don't leave orphan data in the database.

--- mymoney.js
+++ mymoney.js
@@ -1,4 +1,284 @@
 AccountList = new Mongo.Collection('accounts');
+TransactionList = new Mongo.Collection('transactions');
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+
+    // Insert dummy test data
+    if (AccountList.find().count() == 0) {
+      console.log("Add initial test data to db");
+      var gId = Meteor.call('addUser', {username :"Gracelyn", password:"123456"}, "savings");
+      Meteor.call('creditOrDebitAccount', gId, 1000, "Initial Balance", "02/01/2015");
+      Meteor.call('creditOrDebitAccount', gId, -30, "Cell Phone", "03/01/2015");
+      Meteor.call('creditOrDebitAccount', gId, 60, "Grades", "03/09/2015");
+      Meteor.call('creditOrDebitAccount', gId, 204, "Babysitting", "03/03/2015");
+      var cId = Meteor.call('addUser', {username :"Christian", password:"123456"}, "savings");
+      Meteor.call('creditOrDebitAccount', cId, 627, "Initial Balance", "02/01/2015");
+      Meteor.call('creditOrDebitAccount', cId, -30, "Cell Phone", "02/01/2015");
+      var transactionIdToVoid = Meteor.call('creditOrDebitAccount', cId, -25, "PC Game", "02/15/2015");
+      Meteor.call('creditOrDebitAccount', cId, -30, "Cell Phone", "03/01/2015");
+      Meteor.call('voidTransaction', cId, 25, transactionIdToVoid);
+      var vId = Meteor.call('addUser', {username :"Victoria", password:"123456"}, "savings");
+    }
+  });
+
+  Meteor.methods({
+    'addUser': function(options, type){
+      Accounts.createUser(options);
+      var userId = AccountList.insert({
+        userName: options.username,
+        type: type
+      });
+      return userId;
+    },
+    'removeUser': function(user) {
+      var userName = AccountList.findOne(user).userName;
+      AccountList.remove(user);
+      TransactionList.find({user: user}).forEach (
+        function(transactionId) {
+          var amount = TransactionList.findOne(transactionId).amount;
+          //console.log("Remove trans id: " + transactionId + " for: " + userName + " by " + amount );
+          TransactionList.remove(transactionId);
+        }
+      );
+      var u = Meteor.users.findOne({username:userName});
+      Meteor.users.remove({_id:u._id});
+    },
+    'creditOrDebitAccount': function(selectedUser, amount, descr, date) {
+      AccountList.update(selectedUser, {$inc: {balance: amount} });
+      var transactionId = TransactionList.insert(
+        {
+          date: date,
+          amount: amount,
+          subAccount: "savings",
+          description: descr,
+          user: selectedUser
+        }
+      );
+      return transactionId;
+    },
+    // Only called when voiding a transaction
+    'voidTransaction': function(selectedUser, amount, transactionId) {
+      AccountList.update(selectedUser, {$inc: {balance: amount} });
+      //console.log("tId: " + transactionId);
+      var voidId = TransactionList.insert(
+        {
+          date: Date(),
+          amount: amount,
+          subAccount: "savings",
+          description: "Voided transaction " + transactionId,
+          user: selectedUser
+        }
+      );
+      //console.log("vId: " + voidId);
+      TransactionList.update(
+        transactionId,
+        {$set:
+          {description: TransactionList.findOne(transactionId).description
+            + " - Voided by " + voidId}
+        }
+      );
+    },
+    'removeTransaction': function(transactionId) {
+      TransactionList.remove(transactionId);
+//      Accounts.remove(user);
+    },
+  });
+}
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+  Session.set('onAdminPage', false);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  //
+  // addChild Template helpers and events
+  //
+  Template.addChild.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.addChild.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    },
+    'submit form': function(e) {
+      var userName = e.target.userName.value;
+      var userPassword = e.target.userPassword.value;
+      //var userBalance = Number(e.target.userBalance.value);
+      //console.log("Add child: " + userName + ", " + userPassword); // + ", " + userBalance );
+
+      e.preventDefault();
+      var options = {
+        username: userName,
+        password: userPassword
+      };
+      var type = "savings";
+      Meteor.call('addUser', options, type);
+/*
+      AccountList.insert({
+        userName: userName,
+        type: "savings",
+        //balance: userBalance
+      });
+*/
+      e.target.userName.value = null;
+      e.target.userPassword.value = null;
+      //e.target.userBalance.value = null;
+    }
+  });
+
+  //
+  // creditDebit Template helpers and events
+  //
+  Template.creditDebit.events({
+    'submit form': function(e) {
+      var selectedUser = Session.get('selectedUser');
+      var descr = e.target.descr.value;
+      var date = e.target.datepicker.value;
+      var amount = Number(e.target.amount.value);
+      var creditOrDebit = (amount<0) ? "Debit " : "Credit ";
+      //console.log(creditOrDebit + AccountList.findOne(selectedUser).userName + "'s account by " + amount + ", " + descr + " on " + date );
+
+      Meteor.call('creditOrDebitAccount', selectedUser, amount, descr, date);
+      //e.target.amount.value = null;
+      //e.target.descr.value = null;
+    },
+    'focus #datepicker': function () {
+      // show datepicker
+      $('#datepicker').datepicker('show');
+    }
+  });
+
+  Template.creditDebit.rendered = function() {
+    // initialize datepicker
+    $('#datepicker').datepicker({
+      format: 'm, dd, yyyy'
+    });
+  };
+
+  //
+  // Admin Template helpers and events
+  //
+  Template.Admin.events({
+    'click .remove': function(){
+      var selectedUser = this._id;
+      //console.log("User: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (confirm("Are you sure you want to remove user?")) {
+        Meteor.call('removeUser', selectedUser);
+      }
+    }
+  });
+
+  Template.Admin.helpers({
+    user: function() {
+      return AccountList.find();
+    }
+  });
+
+  //
+  // showAccounts Template helpers and events
+  //
+  Template.showAccounts.helpers({
+    user: function() {
+      return AccountList.find();
+    },
+    userBalance: function() {
+      var userId = this._id;
+      var balance = AccountList.findOne(userId).balance;
+      if (balance == null) {
+        return 0;
+      }
+      else {
+        return balance;
+      }
+
+    },
+    'selectedUser': function(){
+      var userId = this._id;
+      var selectedUser = Session.get('selectedUser');
+      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (userId == selectedUser) {
+        // return this class so CSS can highlight the selected player
+        return "selected"
+      }
+    },
+    'onAdminPage': function(){
+      var onAdminPage = Session.get('onAdminPage');
+      return onAdminPage;
+    }
+  });
+
+  Template.showAccounts.events({
+    'click .user': function(event){
+      var userId = this._id;
+      Session.set('selectedUser', userId);
+    },
+
+  });
+
+  //
+  // Accounts Template helpers and events
+  //
+  Template.Accounts.helpers({
+    'showSelectedUser': function(){
+      var userId = this._id;
+      var selectedUser = Session.get('selectedUser');
+      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
+      if (userId == selectedUser) {
+      }
+      return AccountList.findOne(selectedUser);
+    },
+  });
+
+  //
+  // transactions Template helpers and events
+  //
+  Template.transactions.events({
+    'click .void': function(){
+      var transactionId = this._id;
+
+      var selectedUser = Session.get('selectedUser');
+      var amount = -(TransactionList.findOne(transactionId).amount);
+      var creditOrDebit = (amount<0) ? "Debit " : "Credit ";
+      //console.log("id: " + selectedUser + " " + creditOrDebit + AccountList.findOne(selectedUser).userName + "'s account by " + amount );
+
+      if (confirm("Are you sure you want to void this transaction?")) {
+        Meteor.call('voidTransaction', selectedUser, amount, transactionId);
+        //Meteor.call('removeTransaction', transactionId);
+      }
+    }
+  });
+
+  Template.transactions.helpers({
+    transaction: function() {
+      var selectedUser = Session.get('selectedUser');
+      return TransactionList.find({user: selectedUser}, {sort: {date: 1}});
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+}
 
 Router.route('/', function () {
   this.render('Home', {data: {title: 'My Title'}});
@@ -9,12 +289,15 @@ Router.route('/profile', function () {
 });
 
 Router.route('/accounts', function () {
+  Session.set('onAdminPage', false);
   this.render('Accounts');
 });
 
 Router.route('/accounts/:_id', function () {
-  var account = AccountList.findOne({_id: this.params._id});
-  this.render('ShowAccount', {data: account});
+  Session.set('onAdminPage', false);
+  this.render('Accounts');
+//  var account = AccountList.findOne({_id: this.params._id});
+//  this.render('ShowAccount', {data: account});
 });
 
 Router.route('/about', function () {
@@ -31,6 +314,7 @@ Router.route('/recurring', function () {
 });
 
 Router.route('/admin', function () {
+  Session.set('onAdminPage', true);
   this.render('Admin');
 });
 
@@ -47,131 +331,3 @@ Router.route('/restful', {where: 'server'})
     this.response.end('post request\n');
   });
 */
-
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  Template.addChild.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.addChild.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    },
-    'submit form': function(e) {
-      var userName = e.target.userName.value;
-      var userPassword = e.target.userPassword.value;
-      var userBalance = Number(e.target.userBalance.value);
-      console.log("Add child: " + userName + ", " + userPassword + ", " + userBalance );
-
-      e.preventDefault();
-      var options = {
-        username: userName,
-        password: userPassword
-      };
-      Meteor.call('addUser', options)
-
-      AccountList.insert({
-        userName: userName,
-        type: "savings",
-        balance: userBalance
-      });
-
-      e.target.userName.value = null;
-      e.target.userPassword.value = null;
-      e.target.userBalance.value = null;
-    }
-  });
-
-  Template.Admin.events({
-    'click .remove': function(){
-      var selectedUser = this._id;
-      console.log("User: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
-      if (confirm("Are you sure you want to remove player?")) {
-        Meteor.call('removeUser', selectedUser);
-      }
-    }
-  });
-
-  Template.Admin.helpers({
-    user: function() {
-      return AccountList.find();
-    }
-  });
-
-  Template.showAccounts.helpers({
-    user: function() {
-      return AccountList.find();
-    },
-    'selectedUser': function(){
-      var userId = this._id;
-      var selectedUser = Session.get('selectedUser');
-      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
-      if (userId == selectedUser) {
-        // return this class so CSS can highlight the selected player
-        return "selected"
-      }
-    },
-  });
-
-  Template.showAccounts.events({
-    'click .user': function(event){
-      var userId = this._id;
-      Session.set('selectedUser', userId);
-    },
-
-  });
-
-  Template.Accounts.helpers({
-    'showSelectedUser': function(){
-      var userId = this._id;
-      var selectedUser = Session.get('selectedUser');
-      //console.log("Selected user: " + selectedUser + ", " + AccountList.findOne(selectedUser).userName);
-      if (userId == selectedUser) {
-      }
-      return AccountList.findOne(selectedUser);
-    },
-  });
-
-
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
-
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-
-  Meteor.methods({
-    'addUser': function(options){
-      Accounts.createUser(options);
-
-    },
-    'removeUser': function(user) {
-      AccountList.remove(user);
-//      Accounts.remove(user);
-    }
-  });
-
-}
